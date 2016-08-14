7386ca1c4eb317e7b9367679c3322e75e3052262
Chris Gloom <christopherevancrawford@gmail.com>
2016-08-14 05:02:52 -0500

    tentative steps towards npc wander AI, currently just goes to a position and stays there

--- NPC_Wander.cs
+++ NPC_Wander.cs
@@ -4,16 +4,18 @@ using System.Collections;
 public class NPC_Wander : MonoBehaviour {
 	Rigidbody2D myBody;
 	Vector2 movPos;
+	Vector2 targetDown;
 
 	// Use this for initialization
 	void Start () {
 		myBody = GetComponent<Rigidbody2D> ();
 	}
-	
-	// Update is called once per frame
+
 	void FixedUpdate () {
-		movPos = transform.position;
-		movPos += Vector2.down;
-		myBody.MovePosition (movPos);
+		targetDown = new Vector2(10f,60f);
+			movPos = Vector2.Lerp(transform.position, targetDown, Time.deltaTime * 1000f);
+			Debug.DrawLine (transform.position, targetDown, Color.green, 1000f);
+			myBody.MovePosition (movPos);
+	
 	}
 }
